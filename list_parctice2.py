b5b83108868f27a1174c3b05ecde073b02fac1c0
Sourabh Kumar <123962510+Ersourabhkumar@users.noreply.github.com>
2025-07-16 22:48:28 +0530

    Add files via upload

---
+++ list_parctice2.py
@@ -0,0 +1,42 @@
+'''1. Write a Python program to find the maximum element in a list of numbers without
+max functions
+numbers = [10, 20, 4, 45, 99]
+The maximum element is: 99'''
+# n= [99,10, 20, 4, 45]
+# s=n[0]
+ 
+# for i in range(len(n)):
+#     if(n[i]>s):
+#         s=n[i]
+     
+# print(s)
+
+# for i in n:
+#     if i>s:
+#        s=i
+# print(s)
+
+'''2. Write a Python program to sum all the elements in a list of numbers.
+a. numbers = [10, 20, 30, 40]
+b. The sum of all elements is: 100'''
+# numbers = [10, 20, 30, 40]
+# sum=0
+# for i in numbers:
+#     sum=sum+i
+# print(sum)
+
+'''3. Write a Python program to reverse a list without reverse or slicing operator
+a. numbers = [1, 2, 3, 4, 5]
+b. Reversed list: [5, 4, 3, 2, 1]'''
+# numbers = [1, 2, 3, 4, 5]
+# r=[]
+# for i in range(len(numbers)-1,-1,-1):
+#     r.append(numbers[i])
+# print(r)
+
+'''4. Write a Python program to sort a list in ascending order without sort functions'''
+
+n= [99,10, 20, 4, 45]
+for i in range(len(n)):
+    
+
