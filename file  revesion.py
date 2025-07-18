513f2e27f6d9c219b73bf35bcb17cc51cccde07f
Sourabh Kumar <123962510+Ersourabhkumar@users.noreply.github.com>
2025-07-19 00:27:31 +0530

    Add files via upload

---
+++ file  revesion.py
@@ -0,0 +1,34 @@
+# file=open("example.txt","w")
+# file.write("i am teji \n i'm from hanumangarh")
+# file.close()
+
+"""if file create in another dictory(folder)"""
+# import os 
+# os.chdir("C:\\Users\\shain\\OneDrive\\Pictures")
+# file=open("change..txt","w")
+# file.write("this file make from os module and change dirctory(chdir)")
+# file.close()
+
+'''read opreration'''
+# file=open("example.txt","r")
+# print(file.read())
+
+"""append """
+# file=open("example.txt","a")
+# file.write("\n this is append opretor ")
+# file.close()
+
+# import m1           # imprt m1 module
+# print(m1.data)
+# m1.add(1,2,3,4,5).
+
+'''calling module in diffrent'''
+# from m1 import add,data
+# print(data)
+# add(1,2,34,5)
+
+"""built in module"""
+# math
+# Sys
+# os
+# datetime
