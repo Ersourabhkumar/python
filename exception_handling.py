cda2cfbbb1209a05a650c1323083ff9926e0fa32
Sourabh Kumar <123962510+Ersourabhkumar@users.noreply.github.com>
2025-07-19 16:11:51 +0530

    Update exception_handling.py

--- exception_handling.py
+++ exception_handling.py
@@ -9,9 +9,78 @@
 # Null or None reference access
 # Network timeouts
 
-try:
-    num1=int(input("Enter the first number :"))
-    num2=int(input("Enter the second number :"))
-    print(num1/num2)
-except Exception as e:
-    print(e)
+# try:
+#     num1=int(input("Enter the first number :"))
+#     num2=int(input("Enter the second number :"))
+#     print(num1/num2)
+# except Exception as e:
+#     print(e)
+
+
+# try:
+#     num1=int(input("Enter the first number :"))
+#     num2=int(input("Enter the second number :"))
+#     print(num1/num2)
+# except ZeroDivisionError:
+#     num1=int(input("Enter the first number :"))
+#     num2=int(input("Enter the second number :"))
+#     print(num1/num2)
+
+# except Exception as e:
+#     print("teji")
+# else:
+#     print("no error occur")
+
+# try:
+#     num1=int(input("Enter the first number :"))
+#     num2=int(input("Enter the second number :"))
+#     print(num1/num2)
+# except Exception as e:
+#     print("teji")
+# else:
+#     print("no error occur")
+# finally:
+#     print("alwayes execute")
+
+
+''' loop'''
+# try:
+#     num1=int(input("Enter the first number :"))
+#     num2=int(input("Enter the second number :"))
+#     print(num1/num2)
+        
+# except ZeroDivisionError:
+#     while True:
+#         try:
+#             num1=int(input("Enter the first number :"))
+#             num2=int(input("Enter the second number :"))
+#             print(num1/num2)
+#         except ZeroDivisionError:
+#             True
+        
+#         else:
+#             break
+
+"""another method"""
+# while True:
+#         try:
+#             num1=int(input("Enter the first number :"))
+#             num2=int(input("Enter the second number :"))
+#             print(num1/num2)
+#         except ZeroDivisionError:
+#             True
+        
+#         else:
+#             break
+
+# while True:
+#         try:
+#             num1=int(input("Enter the first number :"))
+#             num2=int(input("Enter the second number :"))
+#             print(num1/num2)
+#             break
+#         except ZeroDivisionError:
+#             True
+#         except ValueError:
+#             True
+        
