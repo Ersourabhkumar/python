9fa07ee022770b496fcdec886c333901bf481b69
Sourabh Kumar <123962510+Ersourabhkumar@users.noreply.github.com>
2025-07-19 16:13:27 +0530

    Add files via upload

---
+++ file.handling(01).py
@@ -0,0 +1,27 @@
+''' import os
+os.chdir("c:\\users\\asd\\sql")
+file=open("exmple.txt","w")
+file.write("write to regex \n here you will success in you carrier")
+file.close() '''
+
+#file=open("example","w")
+#file.close()
+#file=open("exmple.txt","r")
+#file.write("this is here because of append")
+#print(file.read())
+#file.close()
+
+#file=open("data.txt","a")
+#file.write("this line is here because of append")
+#file.close()
+
+#import os
+#os.rmdir("c:\\users\\asd\\desktop\\cpp6\\data.txt")
+a=int(input())
+for i in range(a):
+    ld=a%10
+    print(ld)
+    a=a//10
+
+
+
