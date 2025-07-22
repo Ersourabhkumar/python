6fb4cd0b7fbae48cb3d0976978318fcc5f385486
Sourabh Kumar <123962510+Ersourabhkumar@users.noreply.github.com>
2025-07-22 09:16:18 +0530

    Update exception_handling.py

--- exception_handling.py
+++ exception_handling.py
@@ -83,4 +83,61 @@
 #             True
 #         except ValueError:
 #             True
-        
+"""use raise to  create exception """
+# try:
+#     age=int(input("Enter age: "))
+#     if age<18:
+#         raise Exception ("age error occured")  #intentionally generate (raise) an exception when we want to
+#     else:
+#         print("eligible for vote")
+# except Exception as e:
+#     print("error-->",e)
+
+# """   """
+# try:
+#     age=int(input("Enter age: "))
+#     if age<18:
+#         raise Exception ("age error occured")  #intentionally generate (raise) an exception when we want to
+#     elif age==18:
+#         raise Exception ("age=18 ")
+#     else:
+#         print("eligible for vote")
+# except Exception as e:
+#     print("error-->",e)
+# """ use this way to short  the code"""
+# try:
+#     age=int(input("Enter age: "))
+#     if age<18:
+#         raise Exception ("age error occured")  #intentionally generate (raise) an exception when we want to
+#     print("eligible for vote")
+# except Exception as e:
+#     print("error-->",e)
+
+"""create a ATM program through Exception handling """
+# print("welcome to atm")
+# amount=1000
+# A=input("press the key(withdrawal,deposite,check balance)")
+# if A=="withdrawal":
+#     E=int(input("Enter amount: "))
+#     try:
+#         if E<=amount:
+#             amount=amount-E
+#             print("secussfully withdrawal renmaining amount=",amount)
+#         else :
+#             raise Exception ("Enter valid amount")
+#     except Exception as e:
+#         print(e)
+# elif A=="deposite":
+#     D=int(input("Enter deposite amount: " ))
+#     try:
+#         if D<=0:
+#             raise Exception ("plz Enter valid input")
+#         else:
+#             amount=D+amount
+#             print("your new balance is : ",amount)
+#     except Exception as e:
+#         print(e)
+# elif A=="check balance":
+#     print(amount)
+# else:
+#     print("plz press valid key")        
