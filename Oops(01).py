505c0c66649559fbc0770d42decd38e0653923ae
Sourabh Kumar <123962510+Ersourabhkumar@users.noreply.github.com>
2025-07-29 10:27:08 +0530

    Add files via upload

---
+++ Oops(01).py
@@ -0,0 +1,64 @@
+"""Oops"""
+#developed ny 1960 to 1970
+#developed by ole  johan dahi and kristen Nygaard at norwegain computer center in 1967
+# when they creat programing language simula
+
+#class--> it is a blueprint for creatinggg objects
+#object --> it is an instance of a class
+
+"""define a class"""
+# class car:
+#     color ='red'
+#     model ="2023"
+
+#     def start(self):
+#         print("booommm")
+#     def stop(self):
+#         print("stop......")
+
+# #Now create objects
+# scorpio=car()
+# # print(scorpio.color) #calling paramiter
+# scorpio.start() #callinng mathod
+
+"""Access"""
+# class car:
+#     color="black"
+#     model="2000"
+    
+#     def start(self):
+#         return "booommmm"
+#     def stop(self):
+#         return "stop"
+# BMW=car()
+# print(BMW.start())
+
+"""change properties for perticuler object"""
+# class car:
+#     color="black"
+#     model="2000"
+    
+#     def start(self):
+#         return "booommmm"
+#     def stop(self):
+#         return "stop"
+# BMW=car()
+# BMW.color="red"
+# print(BMW.color)
+
+"""change class atribute"""
+# class car:
+#     color="black"
+#     model="2000"
+    
+#     def start(self):
+#         return "booommmm"
+#     def stop(self):
+#         return "stop"
+# car.color="white"
+# BMW=car()
+# print(BMW.color)
+
+
+
+
